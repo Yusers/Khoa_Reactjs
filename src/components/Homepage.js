9b3ddd350f0c53e551e3954ddc999cb59e063137
Yusers <tanyusisreal@gmail.com>
2024-05-15 01:03:55 +0700

    first commit

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Test3 } from './Test3';
 import { NavBar } from './NavBar';
+import { Test4 } from './Test4';
 
 export const Homepage = () => {
   return (
     <>
-      <NavBar />
+      <Test4 text={'Hehe'} />
+      <Test4 text={'Heheasdas'} />
+      <Test4 text={'Hehasdase'} />
+      <Test4 text={'Hehasdasde'} />
     </>
   );
 };
---
+++ src/components/Test4.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Test4 = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+};
